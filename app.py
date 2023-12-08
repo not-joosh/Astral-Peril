775fd1ec8ca28512cc6addc601dedb92ffae91f6
IMMOEfr <i.m.mowsome@gmail.com>
2023-12-08 22:01:43 +0800

    updated stuff

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import pygame
 import sys
 import time
-from scripts.entities import UFO
+from scripts.entities import UFO, Enemy
 
 class Game:
     def __init__(self):
@@ -12,7 +12,17 @@ class Game:
         self.screen = pygame.display.set_mode((640, 480))
         self.display = pygame.Surface((640, 480))
         self.clock = pygame.time.Clock()
-
+        self.font = pygame.font.Font(None,36)
+        self.level_clear = self.font.render("MISSION SUCCESS", True, (255, 255, 255))
+        self.level_clear_rect = self.level_clear.get_rect(center=(640 // 2, 480 // 2))
+        self.level_fail = self.font.render("MISSION FAILED", True, (255, 255, 255))
+        self.level_fail_rect = self.level_fail.get_rect(center=(640 // 2, 480 // 2))
+        image = pygame.image.load("./data/images/player/earth.png")
+        self.earth_image = pygame.transform.scale(image, (960, 540))
+        self.earth = Enemy(750,240, self.earth_image)
+        image = pygame.image.load("./data/images/effects/bullet/03_bullet.png")
+        self.attack_image = pygame.transform.scale(image, (20, 20))
+        self.attack = Enemy(640,220, self.attack_image)
         # Load UFO images
         self.ufo_moving_images = []
         self.ufo_idle_images = []
@@ -20,26 +30,26 @@ class Game:
             image_path = f"./Data/images/player/moving/{i:02d}_UFO.png"
             image = pygame.image.load(image_path)
             # Scale down the UFO image
-            image = pygame.transform.scale(image, (50, 50))
+            image = pygame.transform.scale(image, (30, 30))
             self.ufo_moving_images.append(image)
         for i in range(0, 4):
             image_path = f"./Data/images/player/idle/{i:02d}_UFO.png"
             image = pygame.image.load(image_path)
             # Scale down the UFO image
-            image = pygame.transform.scale(image, (50, 50))
+            image = pygame.transform.scale(image, (30, 30))
             self.ufo_idle_images.append(image)
         self.ufo = UFO(15, 120, self.ufo_idle_images, self.ufo_moving_images)
 
     def run(self):
         while True:
             # Setting Fill
-            self.display.fill((100, 149, 237))
+            self.display.fill((0, 0, 0))
             # Listing Event Scenarios
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-                if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.type == pygame.MOUSEBUTTONDOWN and self.ufo.defeat == 0:
                     # If the mouse is moving and the mouse button is pressed, launch the UFO
                     if self.ufo.is_moving:
                         self.ufo.is_moving = False
@@ -52,28 +62,42 @@ class Game:
             if self.ufo.is_moving and self.timer > 0:
                 self.timer -= 1
                 self.ufo.launch(self.ufo.target) # Mouse Coordinate
+            # if self.attack.collide == True:
+            #     print("hit!")
             else:
                 if(self.ufo.is_moving):
                     print(self.ufo.visited_coords)
-                    
+  
                     # print length of list for me in python
                     # print(len(self.ufo.visited_coords))
                     # Maybe call lagrange here because it stopped moving here
                     # We would flush, then call lagrange, and shoot the bitch possibly.
                 self.ufo.is_moving = False
                 self.timer = 50
-                
+            if self.attack.rect.colliderect(self.ufo.rect):
+                print("hit!")
+                self.ufo.defeat = 1    
             # Update
             self.ufo.update()
+            self.attack.update()
 
             # Draw
-            if self.ufo.is_moving:
-                self.display.blit(self.ufo.moving_images[self.ufo.current_frame], self.ufo.rect)
-            else:
-                self.display.blit(self.ufo.idle_images[self.ufo.current_frame], self.ufo.rect)
+            self.display.blit(self.earth.image, self.earth.rect)
+            
+            if self.ufo.defeat == 0 and self.ufo.victory == 0:
+                self.display.blit(self.attack.image, self.attack.rect)
+                if self.ufo.is_moving:
+                    self.display.blit(self.ufo.moving_images[self.ufo.current_frame], self.ufo.rect)
+                else:
+                    self.display.blit(self.ufo.idle_images[1], self.ufo.rect)
+            elif self.ufo.defeat == 1:
+                self.display.blit(self.level_fail, self.level_fail_rect)
+            elif self.ufo.victory == 1:
+                self.display.blit(self.level_clear, self.level_clear_rect)
+
             self.screen.blit(pygame.transform.scale(self.display, (640, 480)), (0, 0))
             pygame.display.update()
-            self.clock.tick(60)  # Decreased the frames per second to 30
+            self.clock.tick(60)  # Decreased the frames per second to 60
 if __name__ == "__main__":
     Game().run()
 
--- scripts/entities.py
+++ scripts/entities.py
@@ -1,6 +1,6 @@
 import pygame
 import random
-
+import math 
 class UFO(pygame.sprite.Sprite):
     def __init__(self, x, y, idle_images, moving_images):
         # Attributes
@@ -10,7 +10,8 @@ class UFO(pygame.sprite.Sprite):
         self.rect = self.idle_images[0].get_rect()
         self.rect.center = (x, y)
         self.current_frame = 0
-
+        self.victory = 0
+        self.defeat = 0
         # Physics Attributes
         self.velocity = [0, 0]
 
@@ -40,7 +41,8 @@ class UFO(pygame.sprite.Sprite):
 
         # Step 6: Update the UFO's position
         self.rect.center = (self.rect.centerx + self.velocity[0], self.rect.centery + self.velocity[1])
-
+        if self.rect.center[0] >= 536:
+            self.victory = 1 
     def update(self):
         # Conditionally Render the UFO's images to be moving or not moving based on self.is_moving boolean
         if self.is_moving:
@@ -50,3 +52,68 @@ class UFO(pygame.sprite.Sprite):
             self.current_frame = (self.current_frame + 1) % len(self.idle_images)
             self.image = self.idle_images[self.current_frame]
             
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, x, y, image):
+        # Attributes
+        # Sprite Image & Position
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        self.velocity = [-10,0]
+        self.aimed.y = 0
+        # self.collide = pygame.rect.colliderect(self.ufo.rect)
+    def extrapolate_next(self, x, data_points): #using Lagrange for AI attacks
+        needs_sum = []   # mutable list of Li(x0)(), Li(x1)(), Li(x2)(), Li(x3)()
+        for i in range(data_points.__len__()):
+            prodOfLi_n = 1
+            temp = 1
+            for j in range(data_points.__len__()):
+                if i != j: 
+                    prodOfLi_n *= (x - data_points[j][0]) / (data_points[i][0] - data_points[j][0])
+                    temp = prodOfLi_n * data_points[i][1] # Li * fx
+            needs_sum.append(temp)
+        fx = 0
+        for i in range(needs_sum.__len__()):
+            fx += needs_sum[i]
+        self.rect.center = (int(x) + self.velocity[0], int(fx) + self.velocity[1])
+        self.rect.x = int(x)
+        self.aimed.y = int(fx)
+
+        #return result
+    
+    def update(self):
+        # Update the enemy's position or state
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
+        if self.rect.x <= 0:
+            self.rect.x = 640
+            self.rect.y = random.randint(50, 480 - 50)
+        pass
+
+# class PowerUp(pygame.sprite.Sprite):
+#     def __init__(self, x, y, image):
+#         # Attributes
+#         # Sprite Image & Position
+#         self.image = image
+#         self.rect = self.image.get_rect()
+#         self.rect.center = (x, y)
+
+#     def generate(SCREEN_WIDTH, SCREEN_HEIGHT, image):
+#         # Create enemy birds
+# #enemy_birds = pygame.sprite.Group()
+#      for _ in range(5):
+#         x = random.randint(640 // 2, 480 - 50)
+#         y = random.randint(50, 480 - 50)
+#         #enemy_bird = Bird(x, y, image)
+#         #enemy_birds.add(enemy_bird)
+
+#     def apply_power(self, player):
+#         # Apply power to the player
+#         pass
+
+#     def update(self):
+#         # Update the power-up's position or state
+#         pass
+    
+
+
